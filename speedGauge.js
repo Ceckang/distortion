bee39f18ebaf3dc4bebab29d49c9a02775e3a53c
Ceckang <cillemus-holm@hotmail.com>
2018-06-01 14:38:39 +0200

    speed gauge - uden slider

--- speedGauge.js
+++ speedGauge.js
@@ -7,26 +7,26 @@ async function loadScript(){
         let mitJson = await jsonObject.json();
 
         console.log(mitJson);
-}
+
 /* Set radius for all circles */
-var r = 250;
-var circles = document.querySelectorAll('.circle');
-var total_circles = circles.length;
-for (var i = 0; i < total_circles; i++) {
+let r = 250;
+let circles = document.querySelectorAll('.circle');
+let total_circles = circles.length;
+for (let i = 0; i < total_circles; i++) {
 circles[i].setAttribute('r', r);
 }
 
 /* Set meter's wrapper dimension */
-var meter_dimension = (r * 2) + 100;
-var wrapper = document.querySelector("#wrapper");
+let meter_dimension = (r * 2) + 100;
+let wrapper = document.querySelector("#wrapper");
 wrapper.style.width = meter_dimension + "px";
 wrapper.style.height = meter_dimension + "px";
 
 /* Add strokes to circles  */
-var cf = 2 * Math.PI * r;
-var semi_cf = cf / 2;
-var semi_cf_1by3 = semi_cf / 3;
-var semi_cf_2by3 = semi_cf_1by3 * 2;
+let cf = 2 * Math.PI * r;
+let semi_cf = cf / 2;
+let semi_cf_1by3 = semi_cf / 3;
+let semi_cf_2by3 = semi_cf_1by3 * 2;
 document.querySelector("#outline_curves")
 .setAttribute("stroke-dasharray", semi_cf + "," + cf);
 document.querySelector("#low")
@@ -41,19 +41,26 @@ document.querySelector("#mask")
 .setAttribute("stroke-dasharray", semi_cf + "," + cf);
 
 /* Bind range slider event*/
-let slider = document.querySelector("#slider");
+//let slider = document.querySelector("#slider");
 let lbl = document.querySelector("#lbl");
 let mask = document.querySelector("#mask");
 let meter_needle =  document.querySelector("#meter_needle");
+let queue = mitJson.inQueue;
+console.log("Q", queue);
+
 
 function range_change_event() {
-let percent = slider.value;
-//let 
-let meter_value = semi_cf - ((percent * semi_cf) / 100);
+//let percent = slider.value;
+let meter_value = semi_cf - ((queue * semi_cf) / 25);
 mask.setAttribute("stroke-dasharray", meter_value + "," + cf);
 meter_needle.style.transform = "rotate(" + 
-    (270 + ((percent * 180) / 100)) + "deg)";
-lbl.textContent = percent + "%";
+    (270 + ((queue * 180) / 100)) + "deg)";
+lbl.textContent = "# in Q: " + queue;
 }
-slider.addEventListener("input", range_change_event);
+//slider.addEventListener("input", range_change_event);
 
+range_change_event();
+}
+setInterval(function(){ 
+    loadScript();    
+}, 10000);
